c62cefe2d3646ef547666cecce372f64d5036bd9
TheTrueBear <no say>
2021-10-20 10:57:52 -0700

    Test

--- main.py
+++ main.py
@@ -10,5 +10,5 @@ image = img.Image("test.png")
 image.transform_size(vec.Vector2(400, 400))
 
 while True:
-    window.render(vec.Vector2(100, 100), img=image)
+    window.render(vec.Vector2(100, 200), img=image)
     window.update()
