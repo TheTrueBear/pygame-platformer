0778ccf7fc0baa69888343f86eafb1fa0cf16265
TheTrueBear <no say>
2021-10-21 14:21:29 -0700

    Added the Player class and updated other things

--- main.py
+++ main.py
@@ -1,20 +1,23 @@
 import mods.vector as vec
 import mods.gfx.window as wind
 import mods.gfx.image as img
+import mods.player.player as player
 import pygame
 
 window_size = vec.Vector2(800, int((800 / 16) * 10))
 window = wind.Window(window_size, name="Game", framerate=30)
 
 image = img.Image("test.png")
-image.transform_size(vec.Vector2(400, 400))
+image.transform_size(vec.Vector2(200, 200))
+
+plr = player.Player(image = image)
 
 y = 0
 x = 0
 
 while True:
-    y += 100 * window.delta
-    window.render(vec.Vector2(100 + x, 100 + y), img=image)
-    if window.is_key_held(pygame.K_x):
-        x += 100 * window.delta
+
+
+    plr.update(window)
+
     window.update()
