db2befed45bfb68bfa352f6209cec1d5a8ad66e2
TheTrueBear <no say>
2021-10-26 10:35:21 -0700

    Commenting

--- mods/objects/entity.py
+++ mods/objects/entity.py
@@ -33,7 +33,7 @@ class SideScrollControls(Controls):
     pass
 
 class BirdseyeControls(Controls):
-    def return_controls(self, window) -> bool:
+    def return_controls(self, window):
         w = False
         a = False
         s = False
@@ -50,26 +50,36 @@ class BirdseyeControls(Controls):
 
         return w, a, s, d
 
+
+#########################################################################
+# PLAYER
+# This is the player class. The player is what the user will control
+# directly. The only parameter you don't need to set is the controls.
+#
 class Player(Entity):
+    # Initiate the player, setting all the data variables such as controls.
     def __init__(self, sprite, collider, hitbox, position, controls=BirdseyeControls(300, 100, pygame.K_w, pygame.K_s, pygame.K_a, pygame.K_d, pygame.K_SPACE)) -> None:
         # Super the abstract Entity class
         super().__init__(sprite, collider, hitbox, position)
 
+        # Set the controls
         self.controls = controls
 
+    # Teleport the player
     def warp_to(self, position):
         self.position = position
 
+    # Move the player. This is using the controls variable.
     def move(self, window):
         w, a, s, d = self.controls.return_controls(window)
 
-        # Up-down
+        # Up-down movement
         if w:
             self.warp_to(Vector2(self.position.x, self.position.y - (self.controls.speed * window.delta)))
         elif s:
             self.warp_to(Vector2(self.position.x, self.position.y + (self.controls.speed * window.delta)))
 
-        # Left-right
+        # Left-right movement
         if a:
             self.warp_to(Vector2(self.position.x - (self.controls.speed * window.delta), self.position.y))
         elif d:
